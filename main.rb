143fb5445832b2795f8c914db880cab78a102f8b
verbaanmatt <verbaanmatt@gmail.com>
2018-02-18 12:21:18 +0100

    create map method assignment 1

--- main.rb
+++ main.rb
@@ -5,3 +5,12 @@ def read_contacts
   contacts_json = File.read("contacts.json")
   return JSON.parse(contacts_json, { symbolize_names: true })
 end
+
+#"Assignment # 1 map your data"
+
+def name_with_phone_number
+    read_contacts.map {|contact| [name => phone ] }
+end
+
+
+puts name_with_phone_number
