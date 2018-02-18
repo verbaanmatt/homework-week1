d5ef973e80d7a2125aa9807b5e614e4b0e9a4c60
verbaanmatt <verbaanmatt@gmail.com>
2018-02-18 13:10:35 +0100

    create reduce 3.2

--- main.rb
+++ main.rb
@@ -32,9 +32,17 @@ puts org_emails
 #"Assignment 3: Count amount of contacts using reduce:"
 
 def phone_number_with_plus_one_count
-    phone_number_with_plus_one.reduce(0){|sum, element| sum + element}
+    phone_number_with_plus_one.reduce(0) do |sum, element|
      sum += 1
    end
 end
 
 puts phone_number_with_plus_one_count
+
+def org_emails_count
+    org_emails.reduce(0) do |sum, element|
+        sum += 1
+    end
+end
+
+puts org_emails_count
